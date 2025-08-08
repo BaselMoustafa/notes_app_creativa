020ffc23a50186313134f26b6016cc71977fa6d4
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-08-08 20:41:16 +0300

    create style folder

---
+++ lib/style/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+
+  static const Color primary = Colors.blueAccent;
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+  static const Color grey = Colors.grey;
+  static const Color green = Colors.green;
+  static const Color red = Colors.red;
+  static const Color transparent = Colors.transparent;
+
+}
---
+++ lib/style/app_text_styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class AppTextStyles {
+
+  static const TextStyle bold14 =TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 14
+  );
+
+  static const TextStyle bold16 =TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 16
+  );
+
+  static const TextStyle bold18 =TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 18
+  );
+
+  static const TextStyle bold20 =TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 20
+  );
+
+  static const TextStyle bold22 =TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 22
+  );
+
+  static const TextStyle regular14 = TextStyle(
+    fontSize: 14
+  );
+
+  static const TextStyle regular16 = TextStyle(
+    fontSize: 16
+  );
+
+  static const TextStyle regular18 = TextStyle(
+    fontSize: 18
+  );
+}
