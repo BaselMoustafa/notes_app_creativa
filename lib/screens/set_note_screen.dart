8cecbcb36a2001926e0252a3cd87d455cd4df7ee
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-08-14 18:16:06 +0300

    custom text form field created

--- lib/screens/set_note_screen.dart
+++ lib/screens/set_note_screen.dart
@@ -4,7 +4,7 @@ import 'package:notes_app_creativa/style/app_colors.dart';
 import 'package:notes_app_creativa/style/app_text_styles.dart';
 import 'package:notes_app_creativa/widgets/custom_text_form_field.dart';
 
-class SetNoteScreen extends StatelessWidget {
+class SetNoteScreen extends StatefulWidget {
   const SetNoteScreen({
     super.key,
     required this.note,
@@ -13,31 +13,57 @@ class SetNoteScreen extends StatelessWidget {
   final Note? note;
 
   @override
+  State<SetNoteScreen> createState() => _SetNoteScreenState();
+}
+
+class _SetNoteScreenState extends State<SetNoteScreen> {
+  final TextEditingController titleController=TextEditingController();
+  final TextEditingController contentController=TextEditingController();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    contentController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         foregroundColor: AppColors.white,
         backgroundColor: AppColors.primary,
         title: Text(
-          note ==null?"Add Note":"Edit Note",
+          widget.note ==null?"Add Note":"Edit Note",
         ),
       ),
 
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Title",
-            style: AppTextStyles.regular22,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            Text(
+              "Title",
+              style: AppTextStyles.regular22,
+            ),
+        
+            CustomTextFormField(
+              controller: titleController,
+              hintText: "Write Your Title Here...",
+            ),
+        
+            Text(
+              "Content",
+              style: AppTextStyles.regular22,
+            ),
 
-          CustomTextFormField(),
-
-          Text(
-            "Content",
-            style: AppTextStyles.regular22,
-          )
-        ],
+            CustomTextFormField(
+              controller: contentController,
+              hintText: "Write Your Content Here...",
+              maxLines: 7,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -1,29 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_creativa/style/app_colors.dart';
 
 class CustomTextFormField extends StatefulWidget {
-  const CustomTextFormField({super.key});
+  const CustomTextFormField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    this.maxLines = 3,
+  });
+
+  final TextEditingController controller;
+  final String hintText;
+  final int maxLines;
 
   @override
   State<CustomTextFormField> createState() => _CustomTextFormFieldState();
 }
 
 class _CustomTextFormFieldState extends State<CustomTextFormField> {
-  TextEditingController controller = TextEditingController();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    controller.addListener((){
-      print("----------------");
-      print(controller.text);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: controller,
+      minLines: 1,
+      maxLines: widget.maxLines,
+      controller: widget.controller,
+      decoration: InputDecoration(
+        hintText:widget.hintText,
+        border: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        enabledBorder: InputBorder.none,
+        errorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.red
+          )
+        ),
+        focusedErrorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.red
+          )
+        ),
+      ),
     );
   }
 }
